132708bccdb79beb0e6c8377eb680bc0f2bb26bc
idris <idris.sabanli@gmail.com>
2020-02-29 00:56:49 +0400

    class based

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ from bs4 import BeautifulSoup
 import requests
 
 
-response = requests.get('https://www.kadr-az.info/bos-vakansiyalar/29942-texniki-mutexessis', headers={'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.106 Safari/537.36'})
+class Scraper():
+    def __init__(self):
+        response = requests.get('https://www.kadr-az.info/bos-vakansiyalar/29942-texniki-mutexessis', headers={'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.106 Safari/537.36'})
+        self.soup = BeautifulSoup(response.content, 'html.parser')
 
-print(response)
-soup = BeautifulSoup(response.content, 'html.parser')
+    def get_requirements(self):
+        return self.soup.select('div.description > dd:nth-child(2) > p')
 
-print(soup.select('div.description > dd:nth-child(2) > p'))
+
+if __name__ == '__main__':
+    scraper = Scraper()
+    print(scraper.get_requirements())
